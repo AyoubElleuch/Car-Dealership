2c676df80996b67cb630d7635f880eaa690ad67b
Mohamed Ayoub Eleuch <m.eleuch@innopolis.university>
2024-01-24 12:18:59 +0300

    api call to login

--- src/components/LoginComponents/LoginForm.vue
+++ src/components/LoginComponents/LoginForm.vue
@@ -2,18 +2,49 @@
     <div>
         <div class="input_wrapper IBM-Plex-Mono">
             <h4>Username</h4>
-            <input  type="text">
+            <input type="text" v-model="username">
         </div>
         <div class="input_wrapper IBM-Plex-Mono">
             <h4>Password</h4>
-            <input type="password">
+            <input type="password" v-model="password">
         </div>
-        <button class="IBM-Plex-Mono">Login</button>
+        <button class="IBM-Plex-Mono" @click="login">Login</button>
     </div>
 </template>
 <script>
 export default{
-
+    data(){
+        return{
+            username: "Admin",
+            password: "Admin12345"
+        }
+    },
+    methods:{
+        login(){
+            if(this.username == "" || this.password == ""){
+                console.log('you cant login');
+            }else{
+                console.log('you can login');
+            }
+            const data = `${this.username}:${this.password}`;
+            console.log('data: ', JSON.stringify(data));
+            fetch("http://127.0.0.1:8000/api/cms/login/", {
+                method: "POST",
+                headers:{
+                    'Content-Type': 'application/json',
+                    'WWW-Authenticate': 'Basic'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response =>{
+                console.log(response);
+            })
+            .catch(error => {
+                console.log('error in login');
+                console.log(error);
+            })
+        }
+    }
 }
 </script>
 <style scoped>
