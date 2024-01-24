0f3a1884d8e8753e7238300ef3152ef80c2d4432
Amine-Trabelsi <aminetr76@gmail.com>
2024-01-24 15:39:11 +0300

    added token verification

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 from .views import CarList
 from .views import CarDetail
 #from .views import MyObtainTokenPairView
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView, TokenVerifyView
 urlpatterns = [
     path('all/', CarList.as_view(), name='carList'),
     path('model/<int:pk>/', CarDetail.as_view(), name='carDetail'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
